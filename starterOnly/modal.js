32e1b3b607eaf559a083ea317d0df9beea1fa128
MosMid <105169046+MosMid@users.noreply.github.com>
2022-07-04 09:50:20 +0200

    Add files via upload

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,14 +6,11 @@ function editNav() {
       x.className = "topnav";
   }
 }
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelector(".close");
-const btnSumbit = document.querySelector(".btn-submit");
-
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,46 +24,102 @@ modalCloseBtn.addEventListener('click', closeModal);
 function closeModal() {
   modalbg.style.display = "none";
 }
-// valid form elements 
-const radioButtons = document.querySelectorAll("input[name='location']");
-// valid form popup
-btnSumbit.addEventListener('click', formValidate)
 
+const first = document.querySelector("#first");
+const last = document.querySelector("#last");
+const email = document.querySelector("#email");
+const date = document.querySelector("#birthdate");
+const quantity = document.querySelector("#quantity");
+const checkTerms = document.querySelector("input[name= 'checkBox']");
+const radio = document.getElementsByName('location');
+//const radio = document.querySelectorAll('input[name=location]:checked');
+const gcond = document.querySelector("#checkbox1");
 
-var i = 0;
-var j = 0;
-function formValidate() {
-  for (let radioButton of radioButtons) {
+let firstErr = document.querySelector("#firstErr");
+let lastErr = document.querySelector("#lastErr");
+let emailErr = document.querySelector("#emailErr");
+let dateErr = document.querySelector("#dateErr");
+let quantErr = document.querySelector("#quantErr");
+let locationErr = document.querySelector("#locationErr");
+let condErr = document.querySelector("#condErr");
+
+let firstval = false;
+let lastval = false;
+let emailval = false;
+let dateval = false;
+let quantval = false;
+let locationval = false;
+let condval = false;
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", function (e) {
+
+  if (/^[a-zA-Z]+$/.test(first.value) && first.value.length >1) {
+		firstErr.innerText = "";
+    firstval = true;
+	} else {
+		firstErr.innerText = "Deux caractères minimum de aA à zZ";
+	}
+
+  if (/^[a-zA-Z]+$/.test(last.value) && last.value.length >1) {
+		lastErr.innerText = "";
+    lastval = true;
+	} else {
+		lastErr.innerText = "Deux caractères minimum de aA à zZ";
+	}
+
+  if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email.value)) {
+		emailErr.innerText = "";
+    emailval = true;
+	} else {
+		emailErr.innerText = "Inserez une adresse email valide!";
+	}
+
+  if (/^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$/.test(date.value)) {
+		dateErr.innerText = "";
+    dateval = true;
+	} else {
+		dateErr.innerText = "Veuillez inserer une date";
+	}
+
+  if (quantity.value.length < 3 && quantity.value.length >= 1) {
+		quantErr.innerText = "";
+    quantval = true;
+	} else {
+		quantErr.innerText = "Veuillez entrer un nombre entre 0 et 99";
+	}
+
+  var i = 0;
+  let checkmark = false;
+  for (let radioButton of radio) {
     i = i+1;
-      if (radioButton.checked) { 
-        j = j+1;
-      }
-      if (i == 6 && j == 1) {
-        alert("Merci ! Votre réservation a été reçue.");
-        break
-      }
-      if (i == 6 && j != 1) {
-        alert("Inscription Invalide : Selectionez un lieu");
-        break
-      }
+    if (radioButton.checked) { 
+      checkmark = true;
+    }
+    if (i == 6 && checkmark == true) {
+      locationErr.innerText = "";
+      locationval = true;
+      break
+    }
+    if (i == 6 && checkmark == false) {
+      locationErr.innerText = "Selectionnez un lieu";
+      break
+    }
   }
-}
 
-const minFirst = document.querySelector("#first");
-const minLast = document.querySelector("#last");
-const emailCheck = document.querySelector("#email");
-const nombreConcours = document.querySelector("#quantity");
-const checkTerms = document.querySelector("#checkbox1");
-
-function formValidate() {
-  let num1 = nombreConcours.value;
-  let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  let numOnly = /\d/;
-  const checkTerms = document.querySelector("input[name= 'checkBox']");
-  if (minFirst.value.length >= 2 && minLast.value.length >= 2 && emailCheck.value.match(validRegex) && nombreConcours.value.match(numOnly) && checkTerms.checked) {
-      alert("Form Valid");
+  if (gcond.checked) { 
+    condErr.innerText = "";
+    condval = true;
   } else {
-      alert("Form InValid");
+    condErr.innerText = "Veuillez accepter les conditions";
   }
-}
+
+  if ( firstval == true && lastval == true && emailval == true && dateval == true && quantval == true && locationval == true && condval == true){
+    alert("Merci ! Votre réservation a été reçue.");
+  } else {
+    e.preventDefault();
+  }
+
+});
 
