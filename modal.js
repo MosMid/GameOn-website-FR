6f277398d29feb9cf790be4f1dad8d847b491eb8
MosMid <105169046+MosMid@users.noreply.github.com>
2022-08-30 15:18:10 +0200

    Update modal.js

--- modal.js
+++ modal.js
@@ -15,6 +15,29 @@ const inscriptioReussie = document.querySelector("#inscriptionReussie");
 const modalBody = document.querySelector(".modal-body");
 const fermerBtn = document.querySelector("#fermer");
 
+const form = document.querySelector("#form");
+const firstName = document.querySelector("#first");
+const lastName = document.querySelector("#last");
+const email = document.querySelector("#email");
+const date = document.querySelector("#birthdate");
+const quantity = document.querySelector("#quantity");
+const locations = document.getElementsByName('location');
+const conditions = document.querySelector("#checkbox1");
+const notification = document.querySelector("#checkbox2");
+
+const firstNameError = document.querySelector("#firstErr");
+const lastNameError = document.querySelector("#lastErr");
+const emailError = document.querySelector("#emailErr");
+const dateError = document.querySelector("#dateErr");
+const quantityError = document.querySelector("#quantErr");
+const locationError = document.querySelector("#locationErr");
+const conditionsError = document.querySelector("#condErr");
+
+// Regex
+const nameReg = /^[a-zA-Z]+$/;
+const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const dateReg = /^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$/;
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
@@ -31,43 +54,11 @@ function closeModal() {
   inscriptioReussie.style.display = "none";
 }
 
-const firstName = document.querySelector("#first");
-const lastName = document.querySelector("#last");
-const email = document.querySelector("#email");
-const date = document.querySelector("#birthdate");
-const quantity = document.querySelector("#quantity");
-const locations = document.getElementsByName('location');
-//const radio = document.querySelectorAll('input[name=location]:checked');
-const conditions = document.querySelector("#checkbox1");
-const notification = document.querySelector("#checkbox2");
-
-let firstNameError = document.querySelector("#firstErr");
-let lastNameError = document.querySelector("#lastErr");
-let emailError = document.querySelector("#emailErr");
-let dateError = document.querySelector("#dateErr");
-let quantityError = document.querySelector("#quantErr");
-let locationError = document.querySelector("#locationErr");
-let conditionsError = document.querySelector("#condErr");
-
-let firstNameIsValid = false;
-let lastNameIsValid = false;
-let emailIsValid = false;
-let dateIsValid = false;
-let quantityIsValid = false;
-let locationIsValid = false;
-let conditionsIsValid = false;
-
-let nameReg = /^[a-zA-Z]+$/;
-let emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-let dateReg = /^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$/;
-
-const form = document.querySelector("#form");
-
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   if (firstName.value.match(nameReg) && firstName.value.length >1) {
 		firstNameError.innerText = "";
-    firstNameIsValid = true;
+    var firstNameIsValid = true;
 	} else {
 		firstNameError.innerText = "Deux caractères minimum de aA à zZ";
     firstNameIsValid = false;
@@ -75,7 +66,7 @@ form.addEventListener("submit", function (e) {
 
   if (lastName.value.match(nameReg) && lastName.value.length >1) {
 		lastNameError.innerText = "";
-    lastNameIsValid = true;
+    var lastNameIsValid = true;
 	} else {
 		lastNameError.innerText = "Deux caractères minimum de aA à zZ";
     firstNameIsValid = false;
@@ -83,7 +74,7 @@ form.addEventListener("submit", function (e) {
 
   if (email.value.match(emailReg)) {
 		emailError.innerText = "";
-    emailIsValid = true;
+    var emailIsValid = true;
 	} else {
 		emailError.innerText = "Inserez une adresse email valide!";
     firstNameIsValid = false;
@@ -91,7 +82,7 @@ form.addEventListener("submit", function (e) {
 
   if (date.value.match(dateReg)) {
 		dateError.innerText = "";
-    dateIsValid = true;
+    var dateIsValid = true;
 	} else {
 		dateError.innerText = "Veuillez inserer une date";
     firstNameIsValid = false;
@@ -99,14 +90,14 @@ form.addEventListener("submit", function (e) {
 
   if (quantity.value.length < 3 && quantity.value.length >= 1) {
 		quantityError.innerText = "";
-    quantityIsValid = true;
+    var quantityIsValid = true;
 	} else {
 		quantityError.innerText = "Veuillez entrer un nombre entre 0 et 99";
     firstNameIsValid = false;
 	}
 
   var i = 0;
-  let checkmark = false;
+  var checkmark = false;
   for (let radioButton of locations) {
     i = i+1;
     if (radioButton.checked) { 
@@ -114,7 +105,7 @@ form.addEventListener("submit", function (e) {
     }
     if (i == 6 && checkmark == true) {
       locationError.innerText = "";
-      locationIsValid = true;
+      var locationIsValid = true;
       break
     }
     if (i == 6 && checkmark == false) {
@@ -126,7 +117,7 @@ form.addEventListener("submit", function (e) {
 
   if (conditions.checked) { 
     conditionsError.innerText = "";
-    conditionsIsValid = true;
+    var conditionsIsValid = true;
   } else {
     conditionsError.innerText = "Veuillez accepter les conditions";
     firstNameIsValid = false;
@@ -147,9 +138,7 @@ form.addEventListener("submit", function (e) {
         break
       }
     }
-    //form.reset();
     modalBody.style.display = "none";
     inscriptioReussie.style.display = "block";
   }
 });
-
