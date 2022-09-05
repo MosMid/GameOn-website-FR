5ab9d4bb9281a9ca663a8aa9990bec8422acbe38
MosMid <105169046+MosMid@users.noreply.github.com>
2022-09-05 23:29:16 +0200

    Add files via upload

--- modal.js
+++ modal.js
@@ -34,9 +34,15 @@ const locationError = document.querySelector("#locationErr");
 const conditionsError = document.querySelector("#condErr");
 
 // Regex
-const nameReg = /^[a-zA-Z]+$/;
+const nameReg = /[a-zA-Zà-ïò-öù-ü]{2,}$/;
 const emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 const dateReg = /^[0-9]{4}-(((0[13578]|(10|12))-(0[1-9]|[1-2][0-9]|3[0-1]))|(02-(0[1-9]|[1-2][0-9]))|((0[469]|11)-(0[1-9]|[1-2][0-9]|30)))$/;
+
+// obtenir la date
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -56,7 +62,7 @@ function closeModal() {
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  if (firstName.value.match(nameReg) && firstName.value.length >1) {
+  if (firstName.value.match(nameReg)) {
 		firstNameError.innerText = "";
     var firstNameIsValid = true;
 	} else {
@@ -64,7 +70,7 @@ form.addEventListener("submit", function (e) {
     firstNameIsValid = false;
 	}
 
-  if (lastName.value.match(nameReg) && lastName.value.length >1) {
+  if (lastName.value.match(nameReg)) {
 		lastNameError.innerText = "";
     var lastNameIsValid = true;
 	} else {
@@ -81,8 +87,26 @@ form.addEventListener("submit", function (e) {
 	}
 
   if (date.value.match(dateReg)) {
-		dateError.innerText = "";
-    var dateIsValid = true;
+    let year = date.value.substring(0,4);
+    let month = date.value.substring(5,7);
+    let day = date.value.substring(8,10);
+    if(yyyy-year > 16){
+      dateError.innerText = "";
+      var dateIsValid = true;
+    } else if(yyyy-year == 16 && mm-month < 0){
+      dateError.innerText = "Vous devez avoir 16 ans ou plus";
+      firstNameIsValid = false;
+    }else if(yyyy-year == 16 && mm-month >= 0 && dd-day < 0){
+      dateError.innerText = "Vous devez avoir 16 ans ou plus";
+      firstNameIsValid = false;
+    } else if(yyyy-year < 16) {
+      dateError.innerText = "Vous devez avoir 16 ans ou plus";
+      firstNameIsValid = false;
+    } else {
+      dateError.innerText = "";
+      var dateIsValid = true;
+    }
+    
 	} else {
 		dateError.innerText = "Veuillez inserer une date";
     firstNameIsValid = false;
@@ -96,22 +120,17 @@ form.addEventListener("submit", function (e) {
     firstNameIsValid = false;
 	}
 
-  var i = 0;
-  var checkmark = false;
-  for (let radioButton of locations) {
-    i = i+1;
-    if (radioButton.checked) { 
-      checkmark = true;
-    }
-    if (i == 6 && checkmark == true) {
-      locationError.innerText = "";
-      var locationIsValid = true;
-      break
-    }
-    if (i == 6 && checkmark == false) {
+  let locationIsValid = false;
+  try {
+    locations.forEach(function(radioButton) {
       locationError.innerText = "Selectionnez un lieu";
-      firstNameIsValid = false;
-      break
+      if (radioButton.checked) throw true;
+    });
+  } 
+  catch (e) {
+    if(e){
+      locationError.innerText = "";
+      locationIsValid = true;
     }
   }
 
@@ -130,14 +149,9 @@ form.addEventListener("submit", function (e) {
     quantity.value = "";
     conditions.checked = false;
     notification.checked = false;
-    i=0
-    for (let radioButton of locations) {
-      i=i+1;
+    locations.forEach(function(radioButton) {
       radioButton.checked = false;
-      if (i == 6){
-        break
-      }
-    }
+    });
     modalBody.style.display = "none";
     inscriptioReussie.style.display = "block";
   }
